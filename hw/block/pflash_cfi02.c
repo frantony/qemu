340c60ea1c607471aa956b5ee9ab0296daf40ec7
Antony Pavlov <antonynpavlov@gmail.com>
2013-08-29 06:25:35 +0400

    Revert "pflash_cfi02.c:"
    This reverts commit 19aa450bd4b58ac800aec5054938884b9e31f5a9.

--- hw/block/pflash_cfi02.c
+++ hw/block/pflash_cfi02.c
@@ -625,12 +625,7 @@ static void pflash_cfi02_realize(DeviceState *dev, Error **errp)
 
     pflash_setup_mappings(pfl);
     pfl->rom_mode = 1;
-
-    if (pfl->mappings > 0) {
-        sysbus_init_mmio(SYS_BUS_DEVICE(dev), &pfl->mem);
-    } else {
-        sysbus_init_mmio(SYS_BUS_DEVICE(dev), &pfl->orig_mem);
-    }
+    sysbus_init_mmio(SYS_BUS_DEVICE(dev), &pfl->mem);
 
     if (pfl->bs) {
         pfl->ro = bdrv_is_read_only(pfl->bs);
