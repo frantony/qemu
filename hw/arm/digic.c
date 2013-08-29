19418e1d43eca17c128ebd01a17756180c92d482
Antony Pavlov <antonynpavlov@gmail.com>
2013-08-29 08:25:30 +0400

    hw/arm: add very initial support for Canon DIGIC SoC
    DIGIC is Canon Inc.'s name for a family of SoC
    for digital cameras and camcorders.
    
    There is no publicly available specification for
    DIGIC chips. All information about DIGIC chip
    internals is based on reverse engineering efforts
    made by CHDK (http://chdk.wikia.com) and
    Magic Lantern (http://www.magiclantern.fm) projects
    contributors.
    
    Also this patch adds initial support for Canon
    PowerShot A1100 IS compact camera.
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ hw/arm/digic.c
@@ -0,0 +1,86 @@
+/*
+ * QEMU model of the Canon SoC.
+ *
+ * Copyright (C) 2013 Antony Pavlov <antonynpavlov@gmail.com>
+ *
+ * This model is based on reverse engineering efforts
+ * made by CHDK (http://chdk.wikia.com) and
+ * Magic Lantern (http://www.magiclantern.fm) projects
+ * contributors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "exec/address-spaces.h"
+#include "hw/sysbus.h"
+#include "hw/boards.h"
+
+typedef struct DigicState {
+    ARMCPU *cpu;
+    MemoryRegion ram;
+} DigicState;
+
+typedef struct {
+    hwaddr ram_size;
+} DigicBoard;
+
+static DigicState *digic4_create(void)
+{
+    DigicState *s = g_new(DigicState, 1);
+
+    s->cpu = cpu_arm_init("arm946");
+    if (!s->cpu) {
+        fprintf(stderr, "Unable to find CPU definition\n");
+        exit(1);
+    }
+
+    return s;
+}
+
+static void digic4_setup_ram(DigicState *s, hwaddr ram_size)
+{
+    memory_region_init_ram(&s->ram, NULL, "ram", ram_size);
+    memory_region_add_subregion(get_system_memory(), 0, &s->ram);
+    vmstate_register_ram_global(&s->ram);
+}
+
+static void init_digic4_board(DigicBoard *board)
+{
+    DigicState *s = digic4_create();
+
+    digic4_setup_ram(s, board->ram_size);
+}
+
+static DigicBoard a1100_board = {
+    .ram_size = 64 * 1024 * 1024,
+};
+
+static void init_a1100(QEMUMachineInitArgs *args)
+{
+    init_digic4_board(&a1100_board);
+}
+
+static QEMUMachine canon_a1100 = {
+    .name = "canon-a1100",
+    .desc = "Canon PowerShot A1100 IS",
+    .init = &init_a1100,
+};
+
+static void digic_register_machines(void)
+{
+    qemu_register_machine(&canon_a1100);
+}
+
+machine_init(digic_register_machines)
